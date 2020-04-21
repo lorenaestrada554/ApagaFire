20b75402a5e5d308fc6e038c9969a0aee1d7f449
Lorena <lorenaestrada554@gmail.com>
2020-04-20 21:17:22 -0500

    HOLA

--- index2.php
+++ index2.php
@@ -8,11 +8,11 @@ $pass='9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62';
 $conexion = pg_connect("host='ec2-18-210-51-239.compute-1.amazonaws.com' dbname=db8k6qk08f7njf port=5432 user=rlerwmudzipqma password=9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62") or die ("Error de Conexión".pg_last_error());
 
 
-$nombre = $_GET['nombre'];
-$apellido = $_GET['descripcion'];
-$email = $_GET['cantidad'];
-$telefono = $_GET['pais'];
-$contra = $_GET['cc'];
+$nombre = $_POST['nombre'];
+$apellido = $_POST['descripcion'];
+$email = $_POST['cantidad'];
+$telefono = $_POST['pais'];
+$contra = $_POST['cc'];
 $total = "'".$nombre."','".$descripcion."','".$cantidad."','".$pais."','".$cc."'";
 $querycont = pg_query($conexion, "INSERT INTO reporte(nombre,descripcion,cantidad,pais,cc) VALUES (".$total.");");
 
