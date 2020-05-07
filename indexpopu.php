058a9c41f09cb7e44def7e7a011a19d54ca72aa0
Lorena <lorenaestrada554@gmail.com>
2020-05-07 17:48:19 -0500

    fff

--- indexpopu.php
+++ indexpopu.php
@@ -7,10 +7,10 @@ $pass='9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62';
 
 $conexion = pg_connect("host='ec2-18-210-51-239.compute-1.amazonaws.com' dbname=db8k6qk08f7njf port=5432 user=rlerwmudzipqma password=9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62") or die ("Error de Conexión".pg_last_error());
 
-$resp = pg_query($conexion,"Select * from reporte where nombre = 'fabrica'");
+$resp = pg_query($conexion,"Select nombre from reporte where nombre = 'fabrica'");
 $resp = pg_fetch_row($resp);
 $can = $resp[0];
-$consulta = "Select * from reporte where cantidad = '".$can."'";
+$consulta = "Select * from reporte where nombre = '".$can."'";
 $resultado = pg_query($conexion, $consulta);
 $reporte = array();
 $cont =0;
