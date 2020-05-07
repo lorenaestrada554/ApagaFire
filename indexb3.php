c04b191d4772090d6892ab1cbcff2e0c57d384ac
Lorena <lorenaestrada554@gmail.com>
2020-05-07 18:18:53 -0500

    eed

--- indexb3.php
+++ indexb3.php
@@ -7,7 +7,7 @@ $pass='9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62';
 
 $conexion = pg_connect("host='ec2-18-210-51-239.compute-1.amazonaws.com' dbname=db8k6qk08f7njf port=5432 user=rlerwmudzipqma password=9dd7af7373d2134c6134814b1355856a65bf5ed339f0e2df78b8ad5a974c1c62") or die ("Error de Conexión".pg_last_error());
 
-$resp = pg_query($conexion,"Select min(cantidad) from reporte");
+$resp = pg_query($conexion,"Select cantidad from reporte where cantidad = '10'");
 $resp = pg_fetch_row($resp);
 $can = $resp[0];
 $consulta = "Select * from reporte where cantidad = '".$can."'";
